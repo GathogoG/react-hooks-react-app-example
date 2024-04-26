b5b7172890aed6a56f80387fe9d7d7165a2697f9
George Gathogo <gathogogeorge11@gmail.com>
2024-04-26 09:14:57 +0300

    2nd commit

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -6,11 +6,12 @@ function ExampleComponent() {
     <div className="example">
       <img
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
-        alt="a deeply impressed man, perhaps thinking 'whoa'"
+        alt="a deely impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
     </div>
   );
 }
+
 
 export default ExampleComponent;
